74f121b50c9557724e075e9a86975c65ffa9349e
Ankita Sachan <ankitasachan93@gmail.com>
2026-04-15 15:48:54 +1200

    added New workflow with sorting

--- pages/InventoryPage.ts
+++ pages/InventoryPage.ts
@@ -4,14 +4,18 @@ export class InventoryPage {
     readonly page: Page  
     readonly addButton: Locator
     readonly cartLinks: Locator
+    readonly dropdown:Locator
+    readonly productPrices:Locator
 
 
 
     constructor(page: Page) {
 
         this.page = page 
-        this.addButton = page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]')
+        this.addButton =  page.locator('button').filter({ hasText: 'Add to cart' }).first()
         this.cartLinks = page.locator('.shopping_cart_link')
+        this.dropdown =   page.getByRole('combobox')
+        this.productPrices = page.locator('[data-test="inventory-item-price"]')
     }
 
     async addtoCart() {
@@ -21,6 +25,17 @@ export class InventoryPage {
 
     async gotoCart(){
         await this.cartLinks.click()
+    }
+
+    async sortby(option:string){
+        await this.dropdown.selectOption(option)
+    }
+
+    async getprices(): Promise<number[]> {
+        const priceTexts= await this.productPrices.allTextContents()
+        return priceTexts.map(price => parseFloat(price.replace('$', '')))
+
+
     }
 
 
--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -8,11 +8,11 @@ test.describe('End to End',()=>{
     test('End to End flow', async({page, loginpage,inventorypage,cartpage,checkoutpage}) => {
         await loginpage.login(process.env.LOGIN_USERNAME!, process.env.LOGIN_PASSWORD!) //Dashboard login with username=satndard_user and pasword=secret_sauce
         await expect(page).toHaveURL("/inventory.html")
+        const itemname = await page.locator('[data-test="inventory-item-name"]').first().textContent()
+        console.log(itemname)
         await inventorypage.addtoCart()
-        await expect( page.locator('[data-test="remove-sauce-labs-bike-light"]')).toBeVisible() 
-        await expect( page.getByText('1', { exact: true })).toBeVisible()
         await inventorypage.gotoCart()
-        await expect( page.locator('[data-test="inventory-item-name"]')).toHaveText('Sauce Labs Bike Light')
+        await expect( page.locator('[data-test="inventory-item-name"]')).toHaveText(itemname!)
         await cartpage.goToCheckout()
         await checkoutpage.fillDetails('Ankita', 'Sachan','0632')
         await checkoutpage.clickContinue()
