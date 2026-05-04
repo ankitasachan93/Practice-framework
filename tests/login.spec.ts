7d0f9c62095df0d7352fbde8f9690561c79f03f0
Ankita Sachan <ankitasachan93@gmail.com>
2026-05-05 10:15:24 +1200

    updated file

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import {expect, test} from '@playwright/test'
+import {expect, test} from 'playwright/test'
 import { LoginPage } from '../pages/LoginPage'
 import { LoginData } from '../data/login.data'
 
